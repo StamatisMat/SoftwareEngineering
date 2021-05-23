dd849048cae18dac1adbacc334c785b1dfb09680
Dimosthenis Georgoulas <dimosgeo99@gmail.com>
2021-05-23 19:15:24 +0300

    Fixed atbash and rot with empty text
    New button has been removed

--- Rot.java
+++ Rot.java
@@ -7,15 +7,18 @@ public class Rot extends Encoding{
 		String encoded_text="";
 		String[] string_to_chars = text.split("");
 		
-		for(String character: string_to_chars) {
-			if(lowercase_alphabet.contains(character)) {
-				encoded_text+=lowercase_alphabet.charAt((lowercase_alphabet.indexOf(character)+13) % lowercase_alphabet.length());			
-			}else if(uppercase_alphabet.contains(character)){
-				encoded_text+=uppercase_alphabet.charAt((uppercase_alphabet.indexOf(character)+13) % uppercase_alphabet.length());
-			}else {
-				encoded_text+=character;
+		if(text!="") {
+			for(String character: string_to_chars) {
+				if(lowercase_alphabet.contains(character)) {
+					encoded_text+=lowercase_alphabet.charAt((lowercase_alphabet.indexOf(character)+13) % lowercase_alphabet.length());			
+				}else if(uppercase_alphabet.contains(character)){
+					encoded_text+=uppercase_alphabet.charAt((uppercase_alphabet.indexOf(character)+13) % uppercase_alphabet.length());
+				}else {
+					encoded_text+=character;
+				}
 			}
 		}
+
 		return encoded_text;
 	}
 }
